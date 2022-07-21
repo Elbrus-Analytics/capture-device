d65eebc050d7d62fbacb078a719530b65d1e7d97
Tobias Schmidt <102216146+Smiddyyy@users.noreply.github.com>
2022-07-21 09:34:28 +0200

    clear all files above MAXFILE count

--- src/elb-capture-postrotate.sh
+++ src/elb-capture-postrotate.sh
@@ -4,8 +4,12 @@
 source .env
 
 # Remove oldest file form DIR "/var/elbrus/capture/pcap/"
-if [ $( ls $PCAP | wc -l ) -gt $MAXFILES ]; then
-	FILE=$PCAP"$(ls -t $PCAP | tail -1)"
-	rm $FILE
-	echo "[$(date +"%Y-%m-%dT%H:%M:%S%z")] DELETED $FILE" >> $LOGFILE
-fi
+while true; do
+  if [ $( ls $PCAP | wc -l ) -gt $MAXFILES ]; then
+  	FILE=$PCAP"$(ls -t $PCAP | tail -1)"
+  	rm $FILE
+  	echo "[$(date +"%Y-%m-%dT%H:%M:%S%z")] DELETED $FILE" >> $LOGFILE
+  else
+    break
+  fi
+done
