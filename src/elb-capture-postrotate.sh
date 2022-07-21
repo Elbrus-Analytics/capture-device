9b9a063fc0e603c91b0239cda775f8bc83012e06
Tobias Schmidt <102216146+Smiddyyy@users.noreply.github.com>
2022-07-21 09:30:34 +0200

    Update elb-capture-postrotate.sh

--- src/elb-capture-postrotate.sh
+++ src/elb-capture-postrotate.sh
@@ -3,11 +3,9 @@
 # Set env
 source .env
 
-
 # Remove oldest file form DIR "/var/elbrus/capture/pcap/"
 if [ $( ls $PCAP | wc -l ) -gt $MAXFILES ]; then
-	FILE=$PCAP"$(ls -t $WORKINGDIR | tail -1)"
+	FILE=$PCAP"$(ls -t $PCAP | tail -1)"
 	rm $FILE
 	echo "[$(date +"%Y-%m-%dT%H:%M:%S%z")] DELETED $FILE" >> $LOGFILE
 fi
-
