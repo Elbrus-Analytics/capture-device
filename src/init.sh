c225bb5979c6dfde44a089e8460bb4e071ecd00a
Tobias Schmidt <102216146+Smiddyyy@users.noreply.github.com>
2022-07-24 14:57:16 +0200

    Update init.sh

--- src/init.sh
+++ src/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-read -p "Do you want to proceed with setup of the 'capture'? (y/n) " yn
+read -p "Do you want to proceed with setup of the 'capture-device'? (y/n) " yn
 case $yn in
     [yY] | "yes" | "Yes" ) break;;
     [nN] | "no" | "No" ) echo exiting...;
